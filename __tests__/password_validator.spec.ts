3511dc927cffe0c8d7f3d2928a43ef0dd4d62f0f
Fumikazu Fujiwara <github@ze.gs>
2020-03-20 12:20:42 +0900

    spec-2

--- __tests__/password_validator.spec.ts
+++ __tests__/password_validator.spec.ts
@@ -1,21 +1,5 @@
 import fc from 'fast-check';
 import validate from '../src/password_validator';
-const char = (charCodeFrom:integer,charCodeTo:integer) => fc.integer(charCodeFrom, charCodeTo).map(String.fromCharCode);
-const filteredStrings = (min:integer,max:integer,filter:RegExp) => {
-    return fc.array(char(33, //'!'
-                         126 //'~'
-                        ).filter(c => filter.test(c)),min,max).map(arr => arr.join(''));
-}
-const numberStrings = (min:integer,max:integer) => filteredStrings(8,20,/^[0-9]$/);
-const alphabetStrings = (min:integer,max:integer) => filteredStrings(8,20,/^[a-zA-Z]$/);
-const symbolStrings = (min:integer,max:integer) => filteredStrings(8,20,/^[^a-zA-Z0-9]$/);
-const properString = (min:integer,max:integer) => { 
-    return filteredStrings(8,20,/^[a-zA-Z0-9!"#$%&'()*+,.\-\/:;<=>?@\[\\\]^_`{|}~]$/).filter( s => {
-        return !/^[0-9]{8,20}$/.test(s) && 
-            !/^[a-zA-Z]{8,20}$/.test(s) && 
-            !/^[!"#$%&'()*+,.\-\/:;<=>?@\[\\\]^_`{|}~]{8,20}$/.test(s);
-    });
-}
 
 describe('spec 01 : the passwords is required', () => {
     test('Passwords is required', () => {
@@ -38,42 +22,6 @@ describe('spec 02 : the password should have a minimum of 8 characters and maxim
             fc.property(fc.string(21,100),(password:string) => {
                 ///console.log(password);
                 expect(validate(password)).toBe(false);
-            })
-        );
-    });
-});
-describe('spec 03 : the password should be mixed alphabets , numbers and printable symbols', () => {
-    test('Passwords only consisting numbers{8,20} should be rejected', () => {
-        fc.assert(
-            fc.property(numberStrings(8,20),(password:string) => {
-                //  console.log(password);
-                expect(validate(password)).toBe(false);
-            })
-        );
-    });
-    test('Passwords only consisting alphabets{8,20} should be rejected', () => {
-        fc.assert(
-            fc.property(alphabetStrings(8,20),(password:string) => {
-                //  console.log(password);
-                expect(validate(password)).toBe(false);
-            })
-        );
-    });
-    test('Passwords only consisting printable symbols{8,20} should be rejected', () => {
-        fc.assert(
-            fc.property(symbolStrings(8,20),(password:string) => {
-                //     console.log(password);
-                expect(validate(password)).toBe(false);
-            })
-        );
-    });
-});
-describe('Positibe test', () => {
-    test('Passwords only consisting proper characters{8,20} should be accepted', () => {
-        fc.assert(
-            fc.property(properString(8,20),(password:string) => {
-                //   console.log(password);
-                expect(validate(password)).toBe(true);
             })
         );
     });
--- src/password_validator.ts
+++ src/password_validator.ts
@@ -1,9 +1,5 @@
 export default function validate(password:string) :boolean {
     return !!password &&
         password.length >= 8 && 
-        password.length <= 20 && 
-        /^[0-9a-zA-Z!"#$%&'()*+,.\-\/:;<=>?@\[\\\]^_`{|}~]{8,20}$/.test(password) &&
-        !/^[0-9]{8,20}$/.test(password) && 
-        !/^[a-zA-Z]{8,20}$/.test(password) && 
-        !/^[!"#$%&'()*+,.\-\/:;<=>?@\[\\\]^_`{|}~]{8,20}$/.test(password);
+        password.length <= 20;
 };
